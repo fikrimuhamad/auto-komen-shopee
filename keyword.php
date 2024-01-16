1555fb33b526d348a83c2549d503c36a92744839
MUHAMAD FIKRI <muhfikri.us@gmail.com>
2024-01-16 18:14:29 +0700

    kata-kata yang difilter untuk bot membalas

---
+++ keyword.php
@@ -0,0 +1,20 @@
+<?php
+        return array(
+        //info data
+        // TEXT KEYWORD => KETERANGAN 
+        // MISALKAN ADA KOMENTAR 
+        //stock etalases 1 : maka yang akan muncul bagian stock, begitu juga yang lainnya
+            'stock' => 'Semua barang ready stock ya kak, silakan diorder kakak.',
+            'diskon' => 'untuk vocher diskon / gratis ongkir silahkan ditonton saja livenya ya kak, nnti muncul popup vochernya tinggal diclaim saja kakak.',
+            'stok' => 'Semua barang ready stock ya kak, silakan diorder kakak.',
+            'spill' => 'Maaf kak ga bisa spill semua ya kak, Bisa langsung cek Ulasan dan Gambar, silakan diorder kakak.',
+            'perkenalkan' => 'Maaf kak ga bisa spill semua ya kak, Bisa langsung cek Ulasan dan Gambar, silakan diorder kakak.',
+            'introduce' => 'Maaf kak ga bisa spill semua ya kak, Bisa langsung cek Ulasan dan Gambar, silakan diorder kakak.',
+            'eta' => 'Maaf kak ga bisa spill semua ya kak, Bisa langsung cek Ulasan dan Gambar, silakan diorder kakak.',
+            'bahan' => 'Kami menggunakan bahan RAYON ya kak, silakan diorder kakak.',
+            'warna' => 'Warna Random dan Bisa dipilih tergantung Etalase ya kak, silakan diorder kakak.',
+            'bb' => 'Semua Daster di sini adalah All Size ya kak, silakan diorder kakak.',
+            'harga' => 'Harga sesuai yang ada di Etalase ya kak, silakan diorder kakak.',
+            'rekaman' => 'Kita live langsung kak buka rekaman, jika komentar kakak belum dibaca mungkin ketiban ya kak oleh yg lain, jadi ditunggu saja kak.',
+            'dibaca' => 'Kita baca kok kak satu-satu komentarnya, jika komentar kakak belum dibaca mungkin ketiban ya kak oleh yg lain, jadi ditunggu saja kak.',
+);
