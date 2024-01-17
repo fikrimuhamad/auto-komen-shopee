bd530de47ba0a5e25a775154e62570a7b74fd186
MUHAMAD FIKRI <muhfikri.us@gmail.com>
2024-01-18 03:23:36 +0700

    MENAMBAHKAN KATA-KATA UNTUK BOT

--- keyword.php
+++ keyword.php
@@ -1,20 +1,24 @@
 <?php
-        return array(
-        //info data
-        // TEXT KEYWORD => KETERANGAN 
-        // MISALKAN ADA KOMENTAR 
-        //stock etalases 1 : maka yang akan muncul bagian stock, begitu juga yang lainnya
-            'stock' => 'Semua barang ready stock ya kak, silakan diorder kakak.',
-            'diskon' => 'untuk vocher diskon / gratis ongkir silahkan ditonton saja livenya ya kak, nnti muncul popup vochernya tinggal diclaim saja kakak.',
-            'stok' => 'Semua barang ready stock ya kak, silakan diorder kakak.',
-            'spill' => 'Maaf kak ga bisa spill semua ya kak, Bisa langsung cek Ulasan dan Gambar, silakan diorder kakak.',
-            'perkenalkan' => 'Maaf kak ga bisa spill semua ya kak, Bisa langsung cek Ulasan dan Gambar, silakan diorder kakak.',
-            'introduce' => 'Maaf kak ga bisa spill semua ya kak, Bisa langsung cek Ulasan dan Gambar, silakan diorder kakak.',
-            'eta' => 'Maaf kak ga bisa spill semua ya kak, Bisa langsung cek Ulasan dan Gambar, silakan diorder kakak.',
-            'bahan' => 'Kami menggunakan bahan RAYON ya kak, silakan diorder kakak.',
-            'warna' => 'Warna Random dan Bisa dipilih tergantung Etalase ya kak, silakan diorder kakak.',
-            'bb' => 'Semua Daster di sini adalah All Size ya kak, silakan diorder kakak.',
-            'harga' => 'Harga sesuai yang ada di Etalase ya kak, silakan diorder kakak.',
-            'rekaman' => 'Kita live langsung kak buka rekaman, jika komentar kakak belum dibaca mungkin ketiban ya kak oleh yg lain, jadi ditunggu saja kak.',
-            'dibaca' => 'Kita baca kok kak satu-satu komentarnya, jika komentar kakak belum dibaca mungkin ketiban ya kak oleh yg lain, jadi ditunggu saja kak.',
+return array(
+    //info data
+    // TEXT KEYWORD => KETERANGAN 
+    // MISALKAN ADA KOMENTAR 
+    //stock etalases 1 : maka yang akan muncul bagian stock, begitu juga yang lainnya
+    "size" => "Size Chart :\nM= LD 85.PJ 64 CM (BB 30-40KG)\nL = LD 100.PJ 69 CM (BB 45-55KG)\nXL= LD 110.PJ 72 CM (BB 60-70KG)\nXXL LD 116.PJ 74 CM (BB 75-85KG)",
+    "spill" => "Maaf kak kita gak bisa spill semua ya kak, Kakak bisa langsung cek Ulasan dan Gambar jika kepo sama produknya.",
+    "ori" => "Bahannya dijamin bagus ya kak, silakan diorder kakak.",
+    "kualitas" => "Untuk kualitas bahannya dijamin bagus ya kak, silakan diorder kakak.",
+    "stock" => "Semua barang selama sesi live ready stock ya kak, silakan diorder kakak.",
+    "diskon" => "untuk vocher diskon / gratis ongkir silahkan ditonton saja livenya ya kak, nnti muncul popup vochernya tinggal diclaim saja kakak.",
+    "stok" => "Selama sesi live, semua barang ready stock ya kak. Langsung dicheckout saja kak",
+    "perkenalkan" => "Maaf kak gak bisa diperlihatkan semua ya kak, Bisa langsung cek Ulasan dan Gambar, silakan diorder kakak.",
+    "introduce" => "Maaf kak kita ga bisa spill semua ya kak, Bisa langsung cek Ulasan dan Gambar kalau penasaran sama produknya",
+    "eta" => "Maaf kak gak bisa diperlihatkan semua ya kak, Bisa langsung cek Ulasan dan Gambar, silakan diorder kakak.",
+    "bahan" => "Produk kami semuanya menggunakan bahan: COTTON COMBAD 30S & 24S ya kak, dijamin nyaman, adem + tidak tembus ya kak. silakan diorder kakak.",
+    "warna" => "Semua produk kami yang ada diEtalase bisa pilih Warna ya kak, silakan diorder kakak.",
+    "bb" => "UKURAN BB:\nM= LD 85.PJ 64 CM (BB 30-40KG)\nL = LD 100.PJ 69 CM (BB 45-55KG)\nXL= LD 110.PJ 72 CM (BB 60-70KG)\nXXL LD 116.PJ 74 CM (BB 75-85KG)",
+    "harga" => "Harga sesuai yang ada di Etalase ya kak, silakan diorder kakak.",
+    "rekaman" => "Kita live langsung kak buka rekaman, jika komentar kakak belum dibaca mungkin ketiban ya kak oleh yg lain, jadi ditunggu saja kak.",
+    "dibaca" => "Mohon maaf kak jika komentar kakak belum dibaca mungkin chatnya ketiban ya kak oleh yg lain, jadi ditunggu ya kak.",
+    "ready" => "Untuk produk selama sesi live untuk stock dijamin tersedia ya kak",
 );
