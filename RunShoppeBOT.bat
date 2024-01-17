3f5a6156554b44888d016910eb020799ce2e593a
MUHAMAD FIKRI <muhfikri.us@gmail.com>
2024-01-18 03:38:59 +0700

    KLIK 2 KALI LANGSUNG RUN GAK PERLU KETIK LAGI

---
+++ RunShoppeBOT.bat
@@ -0,0 +1,11 @@
+@echo off
+SET PHP_PATH=
+FOR /F "tokens=*" %%i IN ('where php') DO SET PHP_PATH=%%i
+
+IF NOT EXIST "%PHP_PATH%" (
+    echo PHP.exe not found.
+    pause
+    exit /b
+)
+
+    start cmd /k php Shopee.php
